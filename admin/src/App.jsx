76ec3d0856ac02f6eb05bc72d9adb7d94c446d1f
Rathimaan <rathimaanraja1@gmail.com>
2026-08-18 19:14:55 +0530

    fix: make API URL dynamic based on host location to support live EC2 deployment

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = window.location.protocol + "//" + window.location.hostname + ":4000"
 
   return (
     <div>
